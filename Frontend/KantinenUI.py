15bccfc256bbf5c1c7d367dee40c1106109479f2
msoehl <33597350+msoehl@users.noreply.github.com>
2025-05-27 18:13:55 +0200

    Kiosk Mode

--- Frontend/KantinenUI.py
+++ Frontend/KantinenUI.py
@@ -3,6 +3,8 @@ from kivy.config import Config
 Config.set('graphics', 'width', '1024')
 Config.set('graphics', 'height', '600')
 Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'borderless', '1')        # Kein Fensterrahmen
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
